90bfb4688147dc23c4546c772f8d18720a8266d4
AioiLight <info@aioilight.space>
2020-03-02 21:31:36 +0900

    OSに追従するテーマに変更

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,15 @@
 module.exports = {
     title: 'Koioto',
     description: 'はつ恋のように新しい音を。',
+    theme: 'default-prefers-color-scheme',
     themeConfig: {
-      sidebar: 'auto'
+      nav: [
+        { text: 'トップページ', link: '/' },
+        { text: 'ルール', link: '/rule/' },
+        { text: 'Twitter', link: 'https://twitter.com/koiotonet/' }
+      ],
+      sidebar: 'auto',
+      smoothScroll: true,
+      repo: 'Koioto/koioto.net',
     }
   }
