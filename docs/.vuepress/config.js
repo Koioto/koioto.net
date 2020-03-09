c645821983725dfb7585469c7737289dd1fc77a9
AioiLight <info@aioilight.space>
2020-03-09 19:33:11 +0900

    いろいろ設定

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,47 @@
 module.exports = {
-    title: 'Koioto',
-    description: 'はつ恋のように新しい音を。',
-    theme: 'default-prefers-color-scheme',
-    themeConfig: {
-      nav: [
-        { text: 'トップページ', link: '/' },
-        { text: 'ルール', link: '/rule/' },
-        { text: 'Discord', link: 'https://discord.gg/kaF5Nc6' }
-      ],
-      sidebar: [
-        '/',
-        '/rule/',
-        '/env/',
-        // '/chart-making/',
-        '/theme/',
-        '/plugin/'
-        // '/config/'
-      ],
-      sidebarDepth: 2,
-      nextLinks: false,
-      prevLinks: false,
-      logo: '/logo/brand.png',
-      smoothScroll: true,
-      repo: 'Koioto'
-    },
-    markdown: {
-      lineNumbers: true,
-      toc: {
-        includeLevel: [1, 2, 3]
-      }
+  base: '/',
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'Koioto',
+      description: 'はつ恋のように新しい音を。',
     }
-  }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/favicon.ico` }],
+  ],
+  theme: 'default-prefers-color-scheme',
+  themeConfig: {
+    nav: [
+      { text: 'トップページ', link: '/' },
+      { text: 'ルール', link: '/rule/' },
+      { text: 'Discord', link: 'https://discord.gg/kaF5Nc6' }
+    ],
+    sidebar: [
+      '/',
+      '/rule/',
+      '/env/',
+      // '/chart-making/',
+      '/theme/',
+      '/plugin/'
+      // '/config/'
+    ],
+    sidebarDepth: 2,
+    nextLinks: false,
+    prevLinks: false,
+    logo: '/logo/brand.png',
+    smoothScroll: true,
+    repo: 'Koioto'
+  },
+  markdown: {
+    lineNumbers: true,
+    toc: {
+      includeLevel: [1, 2, 3]
+    }
+  },
+  plugins: [
+        ['@vuepress/google-analytics', {
+          ga: 'UA-70020192-7'
+    }]
+ ]
+}
