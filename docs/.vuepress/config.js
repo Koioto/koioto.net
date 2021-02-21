f63d25dde726cbc7fbb1e5a81595f98fcc7fea97
AioiLight <info@aioilight.space>
2021-02-22 04:02:03 +0900

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,6 +51,7 @@ module.exports = {
                 'measure-move',
                 'song-folder',
                 'hotreload',
+                'playing-options'
               ]
             }
           ],
