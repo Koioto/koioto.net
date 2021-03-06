26b55a5d02e8c46eb5dbde7a10503b44e93eb55a
AioiLight <info@aioilight.space>
2021-03-07 07:15:01 +0900

    + Download.vue

---
+++ docs/.vuepress/components/Download.vue
@@ -0,0 +1,51 @@
+<template>
+<a :href="link" v-on:click="clicked">
+    <button>
+        <slot>ダウンロード</slot>
+    </button>
+</a>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: String,
+            default: "#"
+        },
+        action: {
+            type: String,
+            default: "download"
+        },
+        category: {
+            type: String,
+            default: "koioto"
+        },
+        label: {
+            type: String,
+            default: "unknown"
+        }
+    },
+    methods: {
+        clicked: function() {
+            ga('send', 'event', action, category, label);
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    background-color: var(--accentColor);
+    border: none;
+    padding: 0.5em;
+    border-radius: 0.2em;
+    outline: none;
+    color: var(--textColor);
+    font-size: 2em;
+    cursor: pointer;
+}
+button:hover {
+    background-color: var(--darken10AccentColor);
+}
+</style>
--- docs/releases/ver.0.39.md
+++ docs/releases/ver.0.39.md
@@ -1,1 +1,3 @@
 # Ver.0.39
+
+<Download link="https://koioto.net" label="Ver.0.39">ダウンロード (x.xx MB)</Download>
