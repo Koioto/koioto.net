6406e89f2c330a918502f83ed415f27f03a0f211
AioiLight <info@aioilight.space>
2021-03-08 04:01:19 +0900

    Create YouTube.vue

---
+++ docs/.vuepress/components/YouTube.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="yt-embed">
+    <iframe width="720" height="405" v-bind:src="url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            default: "5DRYW09ilOo"
+        }
+    },
+    data: function() {
+        return {
+            url: "https://www.youtube.com/embed/" + this.id
+        }
+    }    
+}
+</script>
+
+<style scoped>
+.yt-embed iframe {
+    width: 100%;
+}
+</style>
